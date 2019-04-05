d1e753a57bfedc688ffd0614e8a188d7776a2fc5
moonheart08 <moonheart08@users.noreply.github.com>
2019-04-05 16:18:36 -0600

    Commit flavored spam, anyone?

--- rusty-rv/src/defs.rs
+++ rusty-rv/src/defs.rs
@@ -1,1 +1,2 @@
 pub type Address = u64;
+pub type AddrRange = std::ops::Range<Address>;
--- rusty-rv/src/memory.rs
+++ rusty-rv/src/memory.rs
@@ -1,15 +1,19 @@
+use crate::defs::{AddrRange, Address};
 use bytes::{Bytes, BytesMut};
-use crate::defs::Address;
 
 pub enum MemoryAccessResult {
 	SuccessfulRead(Bytes),
 	SuccessfulWrite(BytesMut),
 	InvalidAddress,
 	DeviceFailure,
+	WriteFailAt(Vec<AddrRange>), // Writes and reads can fail at multiple positions for multiple ranges of addresses.
+	ReadFailAt(Vec<AddrRange>),
 }
 
 pub trait MemoryDevice {
+	fn device_size(&self) -> usize;
 
-	fn read_at(&self, addr: Address) -> Bytes;
-	fn write_at(&self, addr: Address) -> BytesMut;
+	fn read_at(&self, addr: Address) -> MemoryAccessResult;
+
+	fn write_at(&mut self, addr: Address) -> MemoryAccessResult;
 }
